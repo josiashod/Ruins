23c0c9c352dc8c5944ff52dda2a1032aa96b5b98
ArthurMathis <mathis.arthur68@gmail.com>
2023-11-20 23:33:46 +0100

    Version contenant le notBlindMonster

--- Application/components/header/notBlindMonster.h
+++ Application/components/header/notBlindMonster.h
@@ -6,6 +6,7 @@
 #define QUALITE_DE_PROG_NOTBLINDMONSTER_H
 
 #include "monster.h"
+#include "player.h"
 
 class notBlindMonster : monster {
 public:
@@ -13,13 +14,36 @@ public:
       * @brief Constructeur par défaut
       */
     notBlindMonster();
+    /**
+     * @brief Constructeur par valeurs
+     * @param habilityPercentage Son taux d'habilieté au coups critiques
+     */
+    notBlindMonster(int habilityPercentage);
+    /**
+     * @brief Constructeur par valeurs
+     * @param healthPoints Les points de vie du monstre
+     * @param strenghtPoints Les points d'attaque du monstre
+     * @param habilityPercentage Son taux d'habilieté au coups critiques
+     */
+    notBlindMonster(int healthPoints, int strenghtPoints, int habilityPercentage = 90);
 
-    bool isClose(character &c) const;
 
     /**
-      * @brief Réecriture de la méthode virtuelle permettant ainsi à un monstre voyant de se déplacer
-      */
-    void move() override;
+     * @brief Méthode retournant si le joueur est assez proche du monstre
+     * @param c Le joueur
+     * @return true si le joueur est assez proche
+     */
+    bool isClose(character &c) const;
+    /**
+     * @brief Méthode retournant la direction dans laquelle se situe le joueur
+     * @return La direction
+     */
+    int direction(character &c) const;
+    /**
+     * @brief Méthode déplaçant le monstre selon la position du joueur
+     * @param p Le joueur
+     */
+    void move(character &p);
 };
 
 
